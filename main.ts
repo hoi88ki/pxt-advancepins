5ad73a85c8d592ec5a5aa6c21618d2efc3956fa0
KK Yung <hoi88ki@yahoo.com.hk>
2019-11-21 12:42:36 +0800

    main.tx added

---
+++ main.ts
@@ -0,0 +1,19 @@
+/**
+ * Functions for Controlling Pins with vaiable enabled
+ * 
+ * Created on 21 Nov 2019
+ * Developed by KK Yung
+ * 
+ */
+//% color="#AA278D"
+namespace AdvancedPins {
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+
+    //% block="digital write pin $pinNumber to $pinValue"
+    //% pinValue.min=0 pinValue.max=1
+    export function digitalPinOperation(pinNumber: number, pinValue: number) {
+        
+    }
+}
