e260c6b8bfd423f833f8643c81fcd52723aeb90d
KK Yung <hoi88ki@yahoo.com.hk>
2019-11-21 12:48:36 +0800

    Added digitalPinOperation

--- main.ts
+++ main.ts
@@ -11,9 +11,29 @@ namespace AdvancedPins {
     * Computes the famous Fibonacci number sequence!
     */
 
+    var digitalPinList = [
+        DigitalPin.P0,
+        DigitalPin.P1,
+        DigitalPin.P2,
+        DigitalPin.P3,
+        DigitalPin.P4,
+        DigitalPin.P5,
+        DigitalPin.P6,
+        DigitalPin.P7,
+        DigitalPin.P8,
+        DigitalPin.P9,
+        DigitalPin.P10,
+        DigitalPin.P11,
+        DigitalPin.P12,
+        DigitalPin.P13,
+        DigitalPin.P14,
+        DigitalPin.P15,
+        DigitalPin.P16
+    ];
+
     //% block="digital write pin $pinNumber to $pinValue"
     //% pinValue.min=0 pinValue.max=1
     export function digitalPinOperation(pinNumber: number, pinValue: number) {
-        
+        pins.digitalWritePin(digitalPinList[pinNumber],pinValue);
     }
 }
